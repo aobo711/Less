e0b77aa9ba4f88efa3d381575553900d678c9df4
aobo711 <zjtian711@gmail.com>
2010-12-29 17:03:13 +0800

    add some demos pages

---
+++ demos/js/modules.js
@@ -0,0 +1,18 @@
+var serverUrl = "http://ki.ss/less/" , 
+modules = {
+	base : {
+		scriptPath : "http://ki.ss/less/js/" ,
+		crawlerAllowed : 0
+	},
+	entities: {
+		photos: {
+			source : serverUrl + "photos.html",
+			target : "wp"
+		},
+		messages : {
+			source : serverUrl + "messages.html",
+			target : "wp"  
+		}
+	}
+};
+
